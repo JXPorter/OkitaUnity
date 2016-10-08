d8fd4f2a9af9954518f5e0376fbf6f075deb41c9
Justin Molyneaux <justinmolyneaux@gmail.com>
2016-10-08 16:59:50 -0700

    Added my own Vector3 code

---
+++ UsingVariables_3.10.1/Assets/sphereController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sphereController : MonoBehaviour {
+	public float Control;
+	public float OtherControl;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		//transform.position = new Vector3 (Mathf.Sin(Control) * OtherControl, Mathf.Cos(Control) * OtherControl, Control * OtherControl);
+
+		// Alternate way of writing this code
+		Vector3 vec = new Vector3();
+		vec.x = Mathf.Sin (Control) * OtherControl;
+		vec.y = Mathf.Cos (Control) * OtherControl;
+		vec.z = Control * OtherControl;
+
+		transform.position = vec;
+	}
+}
