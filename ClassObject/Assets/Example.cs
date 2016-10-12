1ece76bb96d980fc0204546a506c36532229fb81
Justin Molyneaux <justinmolyneaux@gmail.com>
2016-10-12 12:21:44 -0700

    updated the ClassObject lesson with my own code

--- ClassObject/Assets/Example.cs
+++ ClassObject/Assets/Example.cs
@@ -3,20 +3,40 @@ using System.Collections;
 
 public class Example : MonoBehaviour
 {
+	void Start()
+	{
+		Vector3 vector = new Vector3 (1, 2, 3);
+		transform.position = vector;
+		vector.x = 2.0f;
+		print ("The camera is located at " + vector);
 
-    // Use this for initialization
-    void Start()
-    {
-        Vector3 vector = new Vector3(1, 2, 3);
-        vector.x = 1.0f;
-        transform.position = vector;
-        int i = new int();
-        Debug.Log(i);
-    }
-	
-    // Update is called once per frame
-    void Update()
-    {
+		// we can also assign plain old data types as an object
+		// although POD types like ints, floats, etc don't really need constructors
+		int i = new int();
+		Debug.Log (i);
+	}
 
-    }
+	void Update()
+	{
+		
+	}
+
 }
+
+
+// Original Code
+//    // Use this for initialization
+//    void Start()
+//    {
+//        Vector3 vector = new Vector3(1, 2, 3);
+//        vector.x = 1.0f;
+//        transform.position = vector;
+//        int i = new int();
+//        Debug.Log(i);
+//    }
+//	
+//    // Update is called once per frame
+//    void Update()
+//    {
+//
+//    }
