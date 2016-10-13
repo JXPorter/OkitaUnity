75a4b0064cf32614567d709960680b89c7e117f9
Justin Molyneaux <justinmolyneaux@gmail.com>
2016-10-12 22:25:51 -0700

    FPS game redo

--- FPSControl/Assets/FPSAim.cs
+++ FPSControl/Assets/FPSAim.cs
@@ -3,31 +3,63 @@ using System.Collections;
 
 public class FPSAim : MonoBehaviour
 {
-	// Use this for initialization
 	void Start()
 	{
+
 	}
-	
-	// Update is called once per frame
+
 	float mouseX;
 	float mouseY;
 	public bool InvertedMouse;
 	void Update()
 	{
-		//Vector3 mousePosition = Input.mousePosition;
-		//Debug.Log(mousePosition);
-		//float mouseX = mousePosition.x;
-		//float mouseY = mousePosition.y;
-		//transform.eulerAngles = new Vector3(mouseY * 0.1f, 0, 0);
-		mouseX += Input.GetAxis("Mouse X");
-		if (InvertedMouse)
+		Vector3 mousePosition = Input.mousePosition;
+		mouseX += Input.GetAxis ("Mouse X");
+		Debug.Log ("MouseX: " + mouseX);
+		if (InvertedMouse) 
 		{
-			mouseY += Input.GetAxis("Mouse Y");
-		} else
-		{
-			mouseY -= Input.GetAxis("Mouse Y");
+			mouseY += Input.GetAxis ("Mouse Y");
 		}
-		Debug.Log(mouseX);
-		transform.eulerAngles = new Vector3(mouseY, mouseX, 0);
+		else
+		{
+				mouseY -= Input.GetAxis("Mouse Y");
+		}
+		Debug.Log ("MouseY: " + mouseY);
+		//print ("Mouse Position is :" + mousePosition);
+
+		transform.eulerAngles = new Vector3 (mouseY, mouseX, 0);
+		Debug.Log ("Camera Rotation: " + transform.eulerAngles);
 	}
 }
+
+//// Original Code
+//public class FPSAim : MonoBehaviour
+//{
+//	// Use this for initialization
+//	void Start()
+//	{
+//	}
+//	
+//	// Update is called once per frame
+//	float mouseX;
+//	float mouseY;
+//	public bool InvertedMouse;
+//	void Update()
+//	{
+//		//Vector3 mousePosition = Input.mousePosition;
+//		//Debug.Log(mousePosition);
+//		//float mouseX = mousePosition.x;
+//		//float mouseY = mousePosition.y;
+//		//transform.eulerAngles = new Vector3(mouseY * 0.1f, 0, 0);
+//		mouseX += Input.GetAxis("Mouse X");
+//		if (InvertedMouse)
+//		{
+//			mouseY += Input.GetAxis("Mouse Y");
+//		} else
+//		{
+//			mouseY -= Input.GetAxis("Mouse Y");
+//		}
+//		Debug.Log(mouseX);
+//		transform.eulerAngles = new Vector3(mouseY, mouseX, 0);
+//	}
+//}
--- FPSControl/Assets/FPSMove.cs
+++ FPSControl/Assets/FPSMove.cs
@@ -3,33 +3,62 @@ using System.Collections;
 
 public class FPSMove : MonoBehaviour
 {
-
-	// Use this for initialization
 	void Start()
 	{
-	
+		
 	}
-	
-	// Update is called once per frame
+
 	public float speed = 0.1f;
 	void Update()
 	{
-		if (Input.GetKey(KeyCode.W))
+		if (Input.GetKey (KeyCode.W)) 
 		{
 			transform.position += transform.forward * speed;
 		}
-		if (Input.GetKey(KeyCode.S))
+		if (Input.GetKey (KeyCode.S)) 
 		{
 			transform.position -= transform.forward * speed;
 		}
-		if (Input.GetKey(KeyCode.A))
+		if (Input.GetKey (KeyCode.A))
 		{
 			transform.position -= transform.right * speed;
 		}
-		if (Input.GetKey(KeyCode.D))
+		if (Input.GetKey (KeyCode.D))
 		{
 			transform.position += transform.right * speed;
 		}
 	}
-
 }
+//// Original Code
+//public class FPSMove : MonoBehaviour
+//{
+//
+//	// Use this for initialization
+//	void Start()
+//	{
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	public float speed = 0.1f;
+//	void Update()
+//	{
+//		if (Input.GetKey(KeyCode.W))
+//		{
+//			transform.position += transform.forward * speed;
+//		}
+//		if (Input.GetKey(KeyCode.S))
+//		{
+//			transform.position -= transform.forward * speed;
+//		}
+//		if (Input.GetKey(KeyCode.A))
+//		{
+//			transform.position -= transform.right * speed;
+//		}
+//		if (Input.GetKey(KeyCode.D))
+//		{
+//			transform.position += transform.right * speed;
+//		}
+//	}
+//
+//}
