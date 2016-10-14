cceb8b6d8a5fdbf69a0c3cffa4fdf6f7d52974c4
Justin Molyneaux <justinmolyneaux@gmail.com>
2016-10-13 19:14:11 -0700

    This 2D Array code may be buggy

--- Grid2D/Assets/Grid2D.cs
+++ Grid2D/Assets/Grid2D.cs
@@ -8,50 +8,113 @@ public class Grid2D : MonoBehaviour
 	public GameObject PuzzlePiece;
 	private GameObject[,] Grid;
 
-	// Use this for initialization
 	void Start()
 	{
 		Grid = new GameObject[Width, Height];
-		for (int x = 0; x < Width; x++)
+		for(int x = 0; x < Width; x++)
 		{
-			for (int y = 0; y < Height; y++)
+			for(int y = 0; y < Height; y++)
 			{
-				GameObject go =
-					GameObject.Instantiate(PuzzlePiece) as GameObject;
-				Vector3 position = new Vector3(x, y, 0);
+				GameObject go = GameObject.Instantiate (PuzzlePiece) as GameObject;
+				Vector3 position = new Vector3 (x,y,0);
 				go.transform.position = position;
-				Grid [x, y] = go;
+				Grid[x,y] = go;
 			}
 		}
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
-		Vector3 mPosition =
-			Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector3 mPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition); //converts mousePosition into a point in space near the puzzle pieces
 		UpdatePickedPiece(mPosition);
+
 	}
-	
+
+	// This function is called from Update() and accepts a Vector3 argument.
 	void UpdatePickedPiece(Vector3 position)
 	{
-		
+		// since Grid[x,y] index is an integer value, we should convert Vector3.x and Vector3.y into ints
+		// since when we cast to an int from a float we lose data, we add .5 to the float before it gets cast so that we better
+		// approximate the expected value as an integer.
 		int x = (int)(position.x + 0.5f);
 		int y = (int)(position.y + 0.5f);
-		
-		for (int _x = 0; _x < Width; _x++)
-		{
-			for (int _y = 0; _y < Height; _y++)
-			{
+		Debug.DrawLine (Vector3.zero, position);
+
+		// The puzzle piece should return to white if we are not touching it. The solution is to set all of the objects to white,
+		// and the colore the current object red.
+
+		for (int _x = 0; _x < Width; _x++) {
+			for (int _y = 0; _y < Height; y++) {
 				GameObject go = Grid [_x, _y];
-				go.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+				go.GetComponent<Renderer> ().material.SetColor ("_Color", Color.white);
 			}
 		}
-		
-		if (x >= 0 && y >= 0 && x < Width && y < Height)
-		{
+
+		// These int values can now be used to pick the PuzzlePiece with Grid[x,y]
+		// Checking to see IndexOutOfRangeException caused by player moving the cursor out of the Grid space area
+		if (x >= 0 && y >= 0 && x < Width && y < Height) {
 			GameObject go = Grid [x, y];
-			go.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+			go.GetComponent<Renderer> ().material.SetColor ("_Color", Color.red); // change the selected puzzle piece to red
 		}
+
 	}
+
 }
+
+//// Original Code
+///*
+// * public class Grid2D : MonoBehaviour
+//{
+//	public int Width;
+//	public int Height;
+//	public GameObject PuzzlePiece;
+//	private GameObject[,] Grid;
+//
+//	// Use this for initialization
+//	void Start()
+//	{
+//		Grid = new GameObject[Width, Height];
+//		for (int x = 0; x < Width; x++)
+//		{
+//			for (int y = 0; y < Height; y++)
+//			{
+//				GameObject go =
+//					GameObject.Instantiate(PuzzlePiece) as GameObject;
+//				Vector3 position = new Vector3(x, y, 0);
+//				go.transform.position = position;
+//				Grid [x, y] = go;
+//			}
+//		}
+//	}
+//
+//	// Update is called once per frame
+//	void Update()
+//	{
+//		Vector3 mPosition =
+//			Camera.main.ScreenToWorldPoint(Input.mousePosition);
+//		UpdatePickedPiece(mPosition);
+//	}
+//	
+//	void UpdatePickedPiece(Vector3 position)
+//	{
+//		
+//		int x = (int)(position.x + 0.5f);
+//		int y = (int)(position.y + 0.5f);
+//		
+//		for (int _x = 0; _x < Width; _x++)
+//		{
+//			for (int _y = 0; _y < Height; _y++)
+//			{
+//				GameObject go = Grid [_x, _y];
+//				go.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+//			}
+//		}
+//		
+//		if (x >= 0 && y >= 0 && x < Width && y < Height)
+//		{
+//			GameObject go = Grid [x, y];
+//			go.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+//		}
+//	}
+//}
+//*/
--- MultiDimensionalArray/Assets/MultiDimensionalArray.cs
+++ MultiDimensionalArray/Assets/MultiDimensionalArray.cs
@@ -1,47 +1,95 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
-public class MultiDimensionalArray : MonoBehaviour {
-	public GameObject[] oneDArray;
-	public GameObject[,] mArray;
-	// Use this for initialization
-void Start () {
 
-	GameObject a = new GameObject("A");
-	GameObject b = new GameObject("B");
-	GameObject c = new GameObject("C");
-	GameObject d = new GameObject("D");
-	GameObject e = new GameObject("E");
-	GameObject f = new GameObject("F");
-	GameObject[,] twoDimension = new GameObject[2,3]{ {a, b, c} , {d, e, f} };
-	GameObject[,,] threeDimension = new GameObject[4,3,2]
+public class MultiDimensionalArray : MonoBehaviour
+{
+	void Start()
+	{
+		GameObject a = new GameObject ("A");
+		GameObject b = new GameObject ("B");
+		GameObject c = new GameObject ("C");
+		GameObject d = new GameObject ("D");
+		GameObject e = new GameObject ("E");
+		GameObject f = new GameObject ("F");
+		GameObject[,] twoDimension = new GameObject[2, 3] 
 		{
-			{  {a,b} ,{c,d}, {e,f}  },
-			{  {a,b} ,{c,d}, {e,f}  },
-			{  {a,b} ,{c,d}, {e,f}  },
-			{  {a,b} ,{c,d}, {e,f}  }
+			{ a, b, c }, { d, e, f }
 		};
-	
-	InspectArray( twoDimension );
-}
-	
-	void InspectArray( GameObject[,] gos ) {
-		
-		int columns = gos.GetLength(0);
-		Debug.Log( "Columns: " + columns );
+		InspectArray (twoDimension);
 
-		int rows = gos.GetLength(1);
-		Debug.Log( "Rows: " + rows );
+		/* 
+		 GameObject[,] twoDimension = new GameObject[2,3];
+		for (int i = 0; i < twoDimension.Length; i++) 
+		{
+			Debug.Log (i); // Outputs 0 - 5 in the Console
+		}
+		*/
+	}
+
+	void Update()
+	{
 		
-		for( int c = 0; c < columns; c++ ) {
-			for( int r = 0; r < rows; r++ ) {
-				Debug.Log( gos[c,r].name );
+	}
+
+	void InspectArray(GameObject[,] gos)
+	{
+		int columns = gos.GetLength (0);
+		Debug.Log ("Columns: " + columns);
+		int rows = gos.GetLength (1);
+		Debug.Log ("Rows: " + rows);
+		for (int c = 0; c < columns; c++) 
+		{
+			for(int r=0; r <rows; r++)
+			{
+				Debug.Log (gos[c,r].name);
 			}
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
+
+//// Original Code
+//public class MultiDimensionalArray : MonoBehaviour {
+//	public GameObject[] oneDArray;
+//	public GameObject[,] mArray;
+//	// Use this for initialization
+//void Start () {
+//
+//	GameObject a = new GameObject("A");
+//	GameObject b = new GameObject("B");
+//	GameObject c = new GameObject("C");
+//	GameObject d = new GameObject("D");
+//	GameObject e = new GameObject("E");
+//	GameObject f = new GameObject("F");
+//	GameObject[,] twoDimension = new GameObject[2,3]{ {a, b, c} , {d, e, f} };
+//	GameObject[,,] threeDimension = new GameObject[4,3,2]
+//		{
+//			{  {a,b} ,{c,d}, {e,f}  },
+//			{  {a,b} ,{c,d}, {e,f}  },
+//			{  {a,b} ,{c,d}, {e,f}  },
+//			{  {a,b} ,{c,d}, {e,f}  }
+//		};
+//	
+//	InspectArray( twoDimension );
+//}
+//	
+//	void InspectArray( GameObject[,] gos ) {
+//		
+//		int columns = gos.GetLength(0);
+//		Debug.Log( "Columns: " + columns );
+//
+//		int rows = gos.GetLength(1);
+//		Debug.Log( "Rows: " + rows );
+//		
+//		for( int c = 0; c < columns; c++ ) {
+//			for( int r = 0; r < rows; r++ ) {
+//				Debug.Log( gos[c,r].name );
+//			}
+//		}
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+//}
