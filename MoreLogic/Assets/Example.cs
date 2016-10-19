4ac00c091392ae5fc4144699643afccb01b7b777
Justin Molyneaux <justinmolyneaux@gmail.com>
2016-10-18 18:20:12 -0700

    added comments to More Logic files

--- MoreLogic/Assets/Example.cs
+++ MoreLogic/Assets/Example.cs
@@ -3,36 +3,73 @@ using System.Collections;
 
 public class Example : MonoBehaviour
 {
-    public PrimitiveType primitiveType;
-    GameObject obj;
-    public enum colorType
-    {
-        red,
-        blue,
-        green
-    }
-    public colorType myColor;
+	public enum colorType
+	{
+		red,
+		green,
+		blue
+	}
 
-    // Use this for initialization
-    void Start()
-    {
-        obj = GameObject.CreatePrimitive(primitiveType);
-        if (myColor == colorType.red)
-        {
-            obj.GetComponent<Renderer>().material.color = Color.red;
-        }
-        if (myColor == colorType.blue)
-        {
-            obj.GetComponent<Renderer>().material.color = Color.blue;
-        }
-        if (myColor == colorType.green)
-        {
-            obj.GetComponent<Renderer>().material.color = Color.green;
-        }
-    }
+	public PrimitiveType primitiveType;
+	public colorType myColor;
+	GameObject obj;
 
-    void Update()
-    {
-		obj.GetComponent<Renderer>().material.color = Color.red;
-    }
+
+	void Start()
+	{
+		obj = GameObject.CreatePrimitive (primitiveType);
+		if(myColor == colorType.red)
+		{
+			obj.GetComponent<Renderer> ().material.color = Color.red;
+		}
+		if(myColor == colorType.blue)
+		{
+			obj.GetComponent<Renderer> ().material.color = Color.blue;
+		}
+		if(myColor == colorType.green)
+		{
+			obj.GetComponent<Renderer> ().material.color = Color.green;
+		}
+	}
+
+	void Update()
+	{
+		
+	}
 }
+//// Original Code
+//public class Example : MonoBehaviour
+//{
+//    public PrimitiveType primitiveType;
+//    GameObject obj;
+//    public enum colorType
+//    {
+//        red,
+//        blue,
+//        green
+//    }
+//    public colorType myColor;
+//
+//    // Use this for initialization
+//    void Start()
+//    {
+//        obj = GameObject.CreatePrimitive(primitiveType);
+//        if (myColor == colorType.red)
+//        {
+//            obj.GetComponent<Renderer>().material.color = Color.red;
+//        }
+//        if (myColor == colorType.blue)
+//        {
+//            obj.GetComponent<Renderer>().material.color = Color.blue;
+//        }
+//        if (myColor == colorType.green)
+//        {
+//            obj.GetComponent<Renderer>().material.color = Color.green;
+//        }
+//    }
+//
+//    void Update()
+//    {
+//		obj.GetComponent<Renderer>().material.color = Color.red;
+//    }
+//}
---
+++ MoreLogic/Assets/Monster.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : MonoBehaviour 
+{
+	// class to determine which state monster is in
+	public enum MonsterState
+	{
+		standing,
+		wandering,
+		chasing,
+		attacking
+	}
+	public MonsterState mState;
+
+	// Use this for initialization
+	void Start () 
+	{
+		mState = MonsterState.standing;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(mState == MonsterState.standing)
+		{
+			print ("standing monster");
+		}
+		if(mState == MonsterState.wandering)
+		{
+			print ("wandering monster");
+		}
+		if(mState == MonsterState.chasing)
+		{
+			print ("chasing monster");
+		}
+		if(mState == MonsterState.attacking)
+		{
+			print ("attacking monster");
+		}
+	}
+}
