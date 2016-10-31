e6bafaf298b0d18cb95422dbd0ecdc741ce730ea
Justin Molyneaux <justinmolyneaux@gmail.com>
2016-10-31 16:09:19 -0700

    updated Arrays

--- MoreOnArrays/Assets/Example.cs
+++ MoreOnArrays/Assets/Example.cs
@@ -1,42 +1,104 @@
 ﻿using UnityEngine;
 using System.Collections;
-
 public class Example : MonoBehaviour
 {
-	
-	// Use this for initialization
+
 	void Start()
 	{
-		
+
+//		// Array Practice Code
+//		int[] primes = { 1, 3, 5, 7, 11, 13, 17, 23, 27, 31 };
+//		int[] fibonacci = {0,1,1,2,3,5,8,13,21,34,55,89,144 };
+//		int[] powersOfTwo = { 1,2,4,8,16,32,64,128,256,512,1024 };
+//		ArrayList Numbers = new ArrayList{ primes, fibonacci, powersOfTwo};
+//		int numArrays = Numbers.Count;
+//		for(int i = 0; i < numArrays; i++)
+//		{
+//			int[] Nums = Numbers [i] as int[];
+//			int items = Nums.Length;
+//			for(int j = 0; j < items; j++)
+//			{
+//				int Num = Nums [j];
+//				print (Num);
+//			}
+//		}
 	}
-	
-	// Update is called once per frame
+
 	void Update()
 	{
-		GameObject[] gos = GameObject.FindGameObjectsWithTag("Monster");
-		ArrayList distances = new ArrayList();
-		foreach (GameObject g in gos)
+		GameObject[] gos = GameObject.FindGameObjectsWithTag ("Monster");
+		ArrayList distances = new ArrayList ();
+		//print (gos.Length);
+		foreach(GameObject g in gos)
 		{
 			Vector3 vec = g.transform.position - transform.position;
 			float distance = vec.magnitude;
-			distances.Add(distance);
+			distances.Add (distance);
 		}
-		
-		float closestValue = (float)distances [0];
+		float closestValue = (float)distances[0];
 		GameObject closestObject = gos [0];
-		
-		for (int i = 0; i < gos.Length; i++)
+		for(int i =0; i < gos.Length; i++)
 		{
 			float d = (float)distances [i];
-			if (d <= closestValue)
+			if(d < closestValue)
 			{
 				closestObject = gos [i];
-				closestValue = d;
+				closestValue = (float)distances [i];
 			}
 		}
-        
-		Vector3 up = new Vector3(0, 1, 0);
+		//print (distances.Count);
+
+		Vector3 up = new Vector3 (0,1,0);
 		Vector3 start = closestObject.transform.position;
-		Debug.DrawRay(start, up);
+		Debug.DrawRay (start,up, Color.green);
+
+
+
+
+		GameObject go = GameObject.Find ("Cube");
+		Vector3 CubePosition = go.transform.position;
+		Vector3 Up = new Vector3 (0,1,0);
+		Debug.DrawRay (CubePosition, Up);
 	}
+
 }
+//// Original Code
+//public class Example : MonoBehaviour
+//{
+//	
+//	// Use this for initialization
+//	void Start()
+//	{
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update()
+//	{
+//		GameObject[] gos = GameObject.FindGameObjectsWithTag("Monster");
+//		ArrayList distances = new ArrayList();
+//		foreach (GameObject g in gos)
+//		{
+//			Vector3 vec = g.transform.position - transform.position;
+//			float distance = vec.magnitude;
+//			distances.Add(distance);
+//		}
+//		
+//		float closestValue = (float)distances [0];
+//		GameObject closestObject = gos [0];
+//		
+//		for (int i = 0; i < gos.Length; i++)
+//		{
+//			float d = (float)distances [i];
+//			if (d <= closestValue)
+//			{
+//				closestObject = gos [i];
+//				closestValue = d;
+//			}
+//		}
+//        
+//		Vector3 up = new Vector3(0, 1, 0);
+//		Vector3 start = closestObject.transform.position;
+//		Debug.DrawRay(start, up);
+//	}
+//}
