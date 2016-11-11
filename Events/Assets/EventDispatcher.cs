f1f174f2ee44b150ceb55e2456e40e239ea5255e
Justin Molyneaux <justinmolyneaux@gmail.com>
2016-11-10 17:59:08 -0800

    Events Lesson

--- Events/Assets/EventDispatcher.cs
+++ Events/Assets/EventDispatcher.cs
@@ -1,7 +1,92 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System;
+using System;                      // The EventArgs type is located in System, so we need to add the using System directive. EventArgs are used to pass parameters to anyone
+								   // listening to the event.
+//
+//// EVENTS - Any number of EventListeners can be added to the scene. 
+//// Any number of functions can be added to the event OnEvent in the EventDispatcher class
+////public delegate void EventHandler();      // first we need a delegate to assign any functions to
+//public delegate void ProperEventHandler(object sender, EventArgs e);     // now a delegate with a parameter list
+//
+////class MyEventArgs : EventArgs              // Important to know that customized event arguments allow for specific event info to be passed to anyone listening for this event
+////{
+////	public string MyNumber;
+////	public MyEventArgs()
+////	{
+////		MyNumber = "I just met you";
+////	}
+////}
+//
+//public class EventArgs<T> : EventArgs      // Using a generic type event with our EventArgs class. Rather than having to figure out every situation ahead of time, it's easier to use generic event type
+//{
+//	public EventArgs(T v)             // this does two things: 1. it means we can put any type of data into the EventArgs, 2. we get to pass the generic data to the listener
+//	{
+//		Value = v;
+//	}
+//	public T Value;
+//}
+//public class EventDispatcher : MonoBehaviour
+//{
+//	public event EventHandler OnEvent;   // the keyword event allows us to assign an event variable to assign functions to. 
+//										 // an event means that we can assign a function to a variable. When an event is used all functions assigned to it will be called.
+//	public event ProperEventHandler ProperEvent;
+//	public bool SendEvent;               // a public bool called SendEvent, initialized to false by default
+//	void Start()
+//	{
+//		
+//	}
+//
+//	private bool isClose;
+//	void Update()
+//	{
+////		if(SendEvent)
+////		{
+////			OnEvent ();                     // if SendEvent is true, then it will execute OnEvent() and then SendEvent will be set back to false.
+////			//ProperEvent(this, new MyEventArgs());  // MyEventArgs() is a class so it needs to be instanced with new before it's used. 
+////			// In param list we use this as the sender and new MyEventArgs() as the EventArgs.
+////			ProperEvent(this, new EventArgs<float>(3.14f));  // when ProperEvent() in EventDispatcher is called, ProximityEvent() in EventListner is also called.
+////			SendEvent = false;
+////		}
+//
+//		GameObject[] Lights = GameObject.FindGameObjectsWithTag ("Lights") as GameObject[];
+//		foreach(GameObject l in Lights)
+//		{
+//			Vector3 targetVector = l.transform.position - transform.position;
+//			float distanceToTarget = targetVector.magnitude;
+//			EventListener el = l.GetComponent<EventListener> ();     // we need to make a reference to EventListener in the script to determine whether or not we're close to the
+//																	// particular object or not. Then we send gameObject.transform as the sender.
+//			if(distanceToTarget <= 10 && !el.isClose)
+//			{
+//				ProperEvent (gameObject.transform, new EventArgs<float> (distanceToTarget));
+//				el.isClose = true;
+//			}
+//			if(distanceToTarget > 10 && el.isClose)
+//			{
+//				ProperEvent (gameObject.transform, new EventArgs<float> (distanceToTarget));
+//				el.isClose = false;
+//			}
+//		}
+//		// Here we use events to check to see if we're near the light target. We use the same sort of function to check for our distance away from the target. If we 're close 
+//		// we set isClose to true, and if we're > 10, we set isClose to false. This way we prevent the event from firing every time Update() is evaluated.
+//		GameObject target = GameObject.Find ("Point light");
+//		Vector3 TargetVector = target.transform.position - transform.position;  // making a distance and light intensity calculation every frame is wasteful, and better served by a simple event when the player approaches or leaves the light
+//		float DistanceToTarget = TargetVector.magnitude;
+//		if (DistanceToTarget <= 10 && !isClose) 
+//		{
+//			ProperEvent(this, new EventArgs<float>(DistanceToTarget));
+//			isClose = true;
+//		} 
+//		if(DistanceToTarget > 10 && isClose)
+//		{
+//			ProperEvent (this, new EventArgs<float> (DistanceToTarget));
+//			isClose = false;
+//		}
+//	}
+//}
 
+
+
+// Original Code
 public delegate void ProperEventHandler(object sender,EventArgs e);
 
 public class EventArgs<T> : EventArgs
--- Events/Assets/EventListener.cs
+++ Events/Assets/EventListener.cs
@@ -2,6 +2,61 @@
 using System.Collections;
 using System;
 
+//public class EventListener : MonoBehaviour
+//{
+//	public bool isClose;
+//	void Start()
+//	// In this Start() of the EventListener, we find the EventDispatcher component of the Main Camera where we assigned the EventDispatcher class.
+//	// Once we've found dispatcher in the Main Camera by using GetComponent<EventDispatcher>(), we can then assign the event a new function. 
+//	{
+//		EventDispatcher dispatcher = GameObject.Find ("Main Camera").GetComponent<EventDispatcher> ();
+//		//dispatcher.OnEvent += CallMeMaybe;        // to add a function to Class.Event you use a += and the function name, in this case, dispatcher.OnEvent += CallMeMaybe; without () on the function name.
+//		//dispatcher.ProperEvent += CallMePlease;
+//		// above dispatcher has OnEvent calling the EventListeners CallMeMaybe function and ProperEvent calls the CallMePlease function
+//		dispatcher.ProperEvent += ProximityEvent;    // when ProperEvent() in EventDispatcher is called, ProximityEvent() in EventListner is also called.
+//	}
+////	void CallMeMaybe()
+////	{
+////		Debug.Log ("here's my number");
+////	}
+////
+////	void CallMePlease(object sender, EventArgs e)  // having these params allows us to pass in event specific info
+////	{
+////		Debug.Log (sender);
+////		MyEventArgs args = (MyEventArgs)e;
+////		Debug.Log (args.MyNumber);
+////	}
+//
+//	public virtual void ProximityEvent(object sender, EventArgs e)
+//	{
+//		Transform t = sender as Transform;
+//		Vector3 targetVector = t.position - transform.position;
+//		float distanceToTarget = targetVector.magnitude;
+//		if (distanceToTarget > 10) {
+//			gameObject.GetComponent<Light> ().intensity = 0.1f;
+//		} else {
+//			gameObject.GetComponent<Light> ().intensity = 2.0f;
+//		}
+//		Debug.Log (distanceToTarget);
+////		EventArgs<float> eVal = (EventArgs<float>)e;    // this takes the EventArgs e and cast it to the proper generic version of the function we're looking for.
+////														// EventArgs<float> eVal is the proper form for setting up a variable for a generic type.
+////		if (eVal.Value > 10) {
+////			gameObject.GetComponent<Light> ().intensity = 0.1f;
+////		} else {
+////			gameObject.GetComponent<Light> ().intensity = 2.0f;
+////		}
+////		Debug.Log (eVal.Value);
+//	}
+//	// To clean up after an object is destroyed, you should take the function out of the EventHandler. Use -= ProximityEvent to pull the function back out of the 
+//	// EventDispatcher's event handler. This is important, otherwise there will be a NullReferenceException Error where the function's object used to be in the event handler.
+//	void OnDestroy()
+//	{
+//		EventDispatcher dispatcher = GameObject.Find ("Main Camera").GetComponent<EventDispatcher> ();
+//		dispatcher.ProperEvent -= ProximityEvent;
+//	}
+//}
+
+// Original Code
 public class EventListener : MonoBehaviour
 {
 	public bool isClose;
