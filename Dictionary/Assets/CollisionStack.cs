8e9f79a3fa4049e8aebbd4e5c069b4f1d53b6c73
Justin Molyneaux <justinmolyneaux@gmail.com>
2016-11-12 17:46:42 -0800

    add Dictionary lesson

--- Dictionary/Assets/CollisionStack.cs
+++ Dictionary/Assets/CollisionStack.cs
@@ -1,88 +1,176 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CollisionStack : MonoBehaviour {
+public class CollisionStack : MonoBehaviour
+{
+	// store the stack and array
 	public GameObject[] HitList;
 	public GameObject[] QueueList;
 	public Stack HitStack;
-	public Queue HitQueue;
-	#region Start
+	public Queue HitQueue;         // Queues are similar to Stacks, but they are FIFO - first in first out, instead of LIFO
+
 	void Start()
 	{
-		HitStack = new Stack();
-		HitQueue = new Queue();
+		HitStack = new Stack ();
+		HitQueue = new Queue ();
 	}
-	#endregion
-	
-	
-	void OnTriggerEnter( Collider other )
+	void OnTriggerEnter(Collider other)   // use the incoming parameter other and the game Object that collider is attached to
 	{
-		#region TriggerStack
-		if( !HitStack.Contains( other.gameObject ) )
+		if (!HitStack.Contains (other.gameObject))   // Stack contains a member function called Contains() where we can use other.gameObject to check through entire stack
+			// to see if object has already been added. if not, then we use Push() to add the gameObject component of other to stack.
 		{
-			HitStack.Push( other.gameObject );
-			HitList = new GameObject[HitStack.Count];
-			HitStack.CopyTo(HitList, 0);
+			HitStack.Push (other.gameObject);
+			HitList = new GameObject[HitStack.Count];    // Stack copies its contents for viewing in the Inspector
+			HitStack.CopyTo (HitList, 0);
 		}
-		#endregion
-		#region TriggerQueue
-		if( !HitQueue.Contains( other.gameObject ) )
+
+		// for HitQueue
+		// this does the same thing as the HitStack
+		// when the capsule touches another object its added to the queue, and then copied to a list so that we can see the objects in the Inspector
+		if(!HitQueue.Contains(other.gameObject))
 		{
-			HitQueue.Enqueue( other.gameObject );
-			QueueList = new GameObject[HitQueue.Count];
-			HitQueue.CopyTo(QueueList, 0);
+		    QueueList = new GameObject[HitQueue.Count];
+			HitQueue.Enqueue (other.gameObject);
+			HitQueue.CopyTo (QueueList, 0);
 		}
-		#endregion
 	}
-	
-	void Update () {
-		#region UpdateStack
-		if( HitStack.Count > 0 )
+
+	void Update()
+	{
+		if(HitStack.Count > 0)
 		{
-			GameObject lastObject = HitStack.Peek() as GameObject;
-			Debug.DrawLine( transform.position, lastObject.transform.position );
-			if( HitList[0] == lastObject )
+			GameObject lastObject = HitStack.Peek () as GameObject;      // Peek() observes the last object added to the stack. assigns the data to lastObject
+			Debug.DrawLine (transform.position, lastObject.transform.position);
+			if(HitList[0] == lastObject)
 			{
-				StartCoroutine("popTheStack");
+				StartCoroutine ("popTheStack");  
 			}
 		}
-		#endregion
-		#region UpdateQueue
-		if( HitQueue.Count > 0 )
+
+		// for HitQueue
+		if(HitQueue.Count > 0)
 		{
-			GameObject firstObject = HitQueue.Peek() as GameObject;
-			Debug.DrawLine( transform.position, firstObject.transform.position, Color.red );
-			if( QueueList[0] == firstObject )
+			GameObject firstObject = HitQueue.Peek () as GameObject;
+			Debug.DrawLine (transform.position, firstObject.transform.position, Color.red);
+			if(QueueList[0] == firstObject)
 			{
-				StartCoroutine("dequeueTheQueue");
+				StartCoroutine ("dequeueTheQueue");
 			}
 		}
-		#endregion
+		
 	}
-	#region popStack
-	IEnumerator popTheStack()
+	IEnumerator popTheStack()                  // To remove an object from the stack, we use Pop(). So when a condition is met, we can reduce the stack by one object.
 	{
-		yield return new WaitForSeconds(2);
-		if( HitStack.Count > 0 )
+		// this function will be started as a Coroutine. Every 2 seconds we'll pop the stack and draw a line to the next object at the top of the stack.
+		// After moving the capsule around in the scene, we'll collect some targets to draw a line to. 
+		// Every 2s we move down the list and reduce the number of items in the stack
+		yield return new WaitForSeconds (2);
+		if(HitStack.Count > 0)
 		{
-			HitStack.Pop();
+			HitStack.Pop ();
 			HitList = new GameObject[HitStack.Count];
-			HitStack.CopyTo(HitList, 0);
-			StopCoroutine("popTheStack");
+			HitStack.CopyTo (HitList, 0);
+			StopCoroutine ("popTheStack");
 		}
 	}
-	#endregion
-	#region dequeueQueue
+
 	IEnumerator dequeueTheQueue()
 	{
-		yield return new WaitForSeconds(2);
-		if( HitQueue.Count > 0 )
-		{
-			HitQueue.Dequeue();
+		// When we drag the capsule around the scene we will see a Gizmo line drawn from both the first and the last objects that the capsule touched.
+		yield return new WaitForSeconds (2);
+						if(HitQueue.Count > 0)
+						{
+			HitQueue.Dequeue ();                       // we used Dequeue to cut our queue down every two seconds.
 			QueueList = new GameObject[HitQueue.Count];
-			HitQueue.CopyTo(QueueList, 0);
-			StopCoroutine("dequeueTheQueue");
-		}
+			HitQueue.CopyTo (QueueList, 0);
+			StopCoroutine ("dequeueTheQueue");
+						}
 	}
-	#endregion
 }
+
+//// Original Code
+//public class CollisionStack : MonoBehaviour {
+//	public GameObject[] HitList;
+//	public GameObject[] QueueList;
+//	public Stack HitStack;
+//	public Queue HitQueue;
+//	#region Start
+//	void Start()
+//	{
+//		HitStack = new Stack();
+//		HitQueue = new Queue();
+//	}
+//	#endregion
+//	
+//	
+//	void OnTriggerEnter( Collider other )
+//	{
+//		#region TriggerStack
+//		if( !HitStack.Contains( other.gameObject ) )
+//		{
+//			HitStack.Push( other.gameObject );
+//			HitList = new GameObject[HitStack.Count];
+//			HitStack.CopyTo(HitList, 0);
+//		}
+//		#endregion
+//		#region TriggerQueue
+//		if( !HitQueue.Contains( other.gameObject ) )
+//		{
+//			HitQueue.Enqueue( other.gameObject );
+//			QueueList = new GameObject[HitQueue.Count];
+//			HitQueue.CopyTo(QueueList, 0);
+//		}
+//		#endregion
+//	}
+//	
+//	void Update () {
+//		#region UpdateStack
+//		if( HitStack.Count > 0 )
+//		{
+//			GameObject lastObject = HitStack.Peek() as GameObject;
+//			Debug.DrawLine( transform.position, lastObject.transform.position );
+//			if( HitList[0] == lastObject )
+//			{
+//				StartCoroutine("popTheStack");
+//			}
+//		}
+//		#endregion
+//		#region UpdateQueue
+//		if( HitQueue.Count > 0 )
+//		{
+//			GameObject firstObject = HitQueue.Peek() as GameObject;
+//			Debug.DrawLine( transform.position, firstObject.transform.position, Color.red );
+//			if( QueueList[0] == firstObject )
+//			{
+//				StartCoroutine("dequeueTheQueue");
+//			}
+//		}
+//		#endregion
+//	}
+//	#region popStack
+//	IEnumerator popTheStack()
+//	{
+//		yield return new WaitForSeconds(2);
+//		if( HitStack.Count > 0 )
+//		{
+//			HitStack.Pop();
+//			HitList = new GameObject[HitStack.Count];
+//			HitStack.CopyTo(HitList, 0);
+//			StopCoroutine("popTheStack");
+//		}
+//	}
+//	#endregion
+//	#region dequeueQueue
+//	IEnumerator dequeueTheQueue()
+//	{
+//		yield return new WaitForSeconds(2);
+//		if( HitQueue.Count > 0 )
+//		{
+//			HitQueue.Dequeue();
+//			QueueList = new GameObject[HitQueue.Count];
+//			HitQueue.CopyTo(QueueList, 0);
+//			StopCoroutine("dequeueTheQueue");
+//		}
+//	}
+//	#endregion
+//}
--- Dictionary/Assets/Dictionaries.cs
+++ Dictionary/Assets/Dictionaries.cs
@@ -1,67 +1,148 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
+using System.Collections.Generic;             // this directive includes Dictionary type
 
 public class Dictionaries : MonoBehaviour
 {
+	// declare a new Dictionary called MyDictionary  - this data type allows us to use a string to find a value in the Dictionary
+	// The structure of Dictionary is <first type, second type> where the first type you use is called a key and second type is the value 
+	// that is associated with that key. They key must always be unique throughout the dictionary.
 	public Dictionary<string,int> MyDictionary;
 	public string[] objectNames;
 	public int[] objectCounts;
-	
-	public GameObject[] ObjectStack;
-	// Use this for initialization
-	void Start ()
-	{
-		GameObject[] gos = GameObject.FindObjectsOfType( typeof( GameObject ) ) as GameObject[];
-		#region Dictionary
-		MyDictionary = new Dictionary<string,int>();
-		MyDictionary.Add("Zombies",10);
-		//prints 10;
-		Debug.Log ( MyDictionary["Zombies"] );
-		
-		//int and object
-		Dictionary<int, Object> obs = new Dictionary<int, Object>();
-		Object[] allObjects = GameObject.FindObjectsOfType( typeof( Object ) ) as Object[];
 
-		for( int i = 0; i < allObjects.Length; i++ ) {
-			obs.Add( i, allObjects[i] );
-			Debug.Log( obs[i] );
-		}
-		
-		//lists GameObject names in the scene
-		Dictionary<string, int> SceneDictionary = new Dictionary<string, int>();
-		foreach( GameObject go in gos )
-		{
-			
-			bool containsKey = SceneDictionary.ContainsKey(go.name);
-			if( containsKey )
-			{
-				SceneDictionary[go.name] += 1;
-			}
-			else
-			{
-				SceneDictionary.Add(go.name, 1);
-			}
-		}
-		objectNames = new string[SceneDictionary.Keys.Count];
-		objectCounts = new int[SceneDictionary.Values.Count];
-		SceneDictionary.Keys.CopyTo(objectNames, 0);
-		SceneDictionary.Values.CopyTo(objectCounts, 0);
-		SceneDictionary.Remove("Main Camera");
-		#endregion
-		
-		#region Stack
-		//create a new stack
+	public GameObject[] ObjectStack;
+
+	void Start()
+	{
+//		MyDictionary = new Dictionary<string,int> ();
+//		MyDictionary.Add ("Zombies",10);         // adds a value to the dictionary - pushes a string with associated values into the variable
+//		// prints 10
+//		Debug.Log(MyDictionary["Zombies"]);     // to retrieve values we use ["KeyName"], which looks like we are addressing an array. This returns the associated value.
+//	
+//		Dictionary<int, Object> obs = new Dictionary<int, Object> ();
+//		Object[] allObjects = GameObject.FindObjectsOfType (typeof(Object)) as Object[];
+//		for(int i = 0; i < allObjects.Length; i++)
+//		{
+//			obs.Add(i, allObjects[i]);          // adds each of the objects to the obs dictionary with a key of i and a value of allObjects[i]
+//			Debug.Log (obs[i]);					// prints out all of the objects in the scene to the console panel
+//			//Debug.Log (obs[19]);              // this should print out the name of the object at index 19 of obs dictionary
+//		}
+//
+//		// lists GameObject names in the scene
+//		Dictionary<string, int> SceneDictionary = new Dictionary<string, int>();   // create a new Dictionary called SceneDictionary
+//		// get array of all of the objects in the scene
+//		GameObject[] gos = GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];
+//		// iterate through them and add them to the dictionary
+//		foreach(GameObject go in gos)
+//		{
+//			// check if we've already found an object with the GameObject's name
+//			// the argument passed to ContainsKey must match the type used to initialze the dictionary, in this case a string.
+//			bool containsKey = SceneDictionary.ContainsKey(go.name);
+//
+//			// Logic - if the dictionary already contains this key, then we need to increment the value in the dictionary of that key += 1.
+//			// If the value is new and ContainsKey = false, then we add a new entry to the dictionary.
+//			if (containsKey) {
+//				SceneDictionary [go.name] += 1;
+//			} else {
+//				SceneDictionary.Add (go.name, 1);
+//			}
+//		}
+//		// initialize the size of the string[] and the int[] to the size of the dictionary
+//		SceneDictionary.Remove("Main Camera");             // If we don't want the Main Camera in the list, use this statement before copying the Dictionary to the string and int []s
+//		objectNames = new string[SceneDictionary.Keys.Count];
+//		objectCounts = new int[SceneDictionary.Values.Count];
+//		SceneDictionary.Keys.CopyTo (objectNames, 0);      // assign all of the values of the keys to the objectNames[].
+//		SceneDictionary.Values.CopyTo (objectCounts, 0);   // assign all of the values of the keys to the objectCounts[].
+
+		// STACKS - an array with some added features. You get a Push(), a Pop(), and a Peek() function. The top of the stack is where all the functions perform operations
+		// Push() - adds an object to the top of the stack.
+		// Peek() - lets you look at the contents of the object on top of the stack
+		// Pop() - means to take the item off the top of the stack.
+
+		GameObject[] gos = GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];
+			// create a new stack
 		Stack objectStack = new Stack();
-		//assign objects to the stack using push
-		foreach( GameObject go in gos )
+		// assign objects to the stack using Push()
+		foreach(GameObject go in gos)
 		{
-			objectStack.Push( go );
+			objectStack.Push (go);
 		}
-		//initialize the class scope ObjectStack to view in the inspector panel
+		// initialize the class scope ObjectStack to view in the Inspector panel
 		ObjectStack = new GameObject[objectStack.Count];
-		//copy the stack to the array in Unity
-		objectStack.CopyTo(ObjectStack, 0 );
-		#endregion
+		// copy the stack to the array in Unity
+		objectStack.CopyTo(ObjectStack, 0);
+	}
+
+
+
+	void Update()
+	{
+		
 	}
 }
+
+//// Original Code
+//public class Dictionaries : MonoBehaviour
+//{
+//	public Dictionary<string,int> MyDictionary;
+//	public string[] objectNames;
+//	public int[] objectCounts;
+//	
+//	public GameObject[] ObjectStack;
+//	// Use this for initialization
+//	void Start ()
+//	{
+//		GameObject[] gos = GameObject.FindObjectsOfType( typeof( GameObject ) ) as GameObject[];
+//		#region Dictionary
+//		MyDictionary = new Dictionary<string,int>();
+//		MyDictionary.Add("Zombies",10);
+//		//prints 10;
+//		Debug.Log ( MyDictionary["Zombies"] );
+//		
+//		//int and object
+//		Dictionary<int, Object> obs = new Dictionary<int, Object>();
+//		Object[] allObjects = GameObject.FindObjectsOfType( typeof( Object ) ) as Object[];
+//
+//		for( int i = 0; i < allObjects.Length; i++ ) {
+//			obs.Add( i, allObjects[i] );
+//			Debug.Log( obs[i] );
+//		}
+//		
+//		//lists GameObject names in the scene
+//		Dictionary<string, int> SceneDictionary = new Dictionary<string, int>();
+//		foreach( GameObject go in gos )
+//		{
+//			
+//			bool containsKey = SceneDictionary.ContainsKey(go.name);
+//			if( containsKey )
+//			{
+//				SceneDictionary[go.name] += 1;
+//			}
+//			else
+//			{
+//				SceneDictionary.Add(go.name, 1);
+//			}
+//		}
+//		objectNames = new string[SceneDictionary.Keys.Count];
+//		objectCounts = new int[SceneDictionary.Values.Count];
+//		SceneDictionary.Keys.CopyTo(objectNames, 0);
+//		SceneDictionary.Values.CopyTo(objectCounts, 0);
+//		SceneDictionary.Remove("Main Camera");
+//		#endregion
+//		
+//		#region Stack
+//		//create a new stack
+//		Stack objectStack = new Stack();
+//		//assign objects to the stack using push
+//		foreach( GameObject go in gos )
+//		{
+//			objectStack.Push( go );
+//		}
+//		//initialize the class scope ObjectStack to view in the inspector panel
+//		ObjectStack = new GameObject[objectStack.Count];
+//		//copy the stack to the array in Unity
+//		objectStack.CopyTo(ObjectStack, 0 );
+//		#endregion
+//	}
+//}
